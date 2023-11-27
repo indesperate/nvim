14552a7678f99fcdf004b5d3bdea910a521e53b5
indesperate <indesperate@outlook.com>
2023-11-27 21:23:25 +0800

    small change to noice

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -78,6 +78,9 @@ require("noice").setup({
             ["cmp.entry.get_documentation"] = true,
         },
     },
+    messages = {
+        view = "mini"
+    },
     -- you can enable a preset for easier configuration
     presets = {
         bottom_search = true,         -- use a classic bottom cmdline for search
