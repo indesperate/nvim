59b6a2ae4c16d037304bfe8e76e86700d8cac8b2
indesperate <indesperate@outlook.com>
2023-11-28 07:00:33 +0800

    fix lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,45 +1,37 @@
 -- lsp config
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
-require("mason").setup({
-	ensure_installed = {
-		"vim-language-server",
-		"stylua",
-		"lua-language-server",
-		"ruff-lsp",
-		"shfmt",
-		"taplo",
-		"clang-format",
-	},
+require("mason").setup()
+require("mason-lspconfig").setup({
+    handlers = {
+        function(server_name) -- default handler
+            require("lspconfig")[server_name].setup({
+                capabilities = capabilities,
+            })
+        end,
+        ["lua_ls"] = function()
+            require("lspconfig").lua_ls.setup({
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = "LuaJIT",
+                        },
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            library = {
+                                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                            },
+                            maxPreload = 5000,
+                            preloadFileSize = 10000,
+                        },
+                        telemetry = { enable = false },
+                    },
+                },
+            })
+        end,
+    }
 })
-require("mason-lspconfig").setup()
-require("mason-lspconfig").setup_handlers({
-	function(server_name) -- default handler
-		require("lspconfig")[server_name].setup({
-			capabilities = capabilities,
-		})
-	end,
-	["lua_ls"] = function()
-		require("lspconfig").lua_ls.setup({
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-					},
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						library = {
-							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-							[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-						},
-						maxPreload = 5000,
-						preloadFileSize = 10000,
-					},
-					telemetry = { enable = false },
-				},
-			},
-		})
-	end,
-})
+
