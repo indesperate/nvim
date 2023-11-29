441bc69f7c1635a646491214401e0a3d9fe73ce0
indesperate <indesperate@outlook.com>
2023-11-29 15:34:11 +0800

    add more color

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,36 +2,35 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("mason").setup()
 require("mason-lspconfig").setup({
-    handlers = {
-        function(server_name) -- default handler
-            require("lspconfig")[server_name].setup({
-                capabilities = capabilities,
-            })
-        end,
-        ["lua_ls"] = function()
-            require("lspconfig").lua_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = "LuaJIT",
-                        },
-                        diagnostics = {
-                            globals = { "vim" },
-                        },
-                        workspace = {
-                            library = {
-                                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                                [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                            },
-                            maxPreload = 5000,
-                            preloadFileSize = 10000,
-                        },
-                        telemetry = { enable = false },
-                    },
-                },
-            })
-        end,
-    }
+	handlers = {
+		function(server_name) -- default handler
+			require("lspconfig")[server_name].setup({
+				capabilities = capabilities,
+			})
+		end,
+		["lua_ls"] = function()
+			require("lspconfig").lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = {
+								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+								[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+							},
+							maxPreload = 5000,
+							preloadFileSize = 10000,
+						},
+						telemetry = { enable = false },
+					},
+				},
+			})
+		end,
+	},
 })
-
