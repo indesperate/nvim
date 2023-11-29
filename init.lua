d1d2779a4acd393c63af7caec382ccdfde5793b6
indesperate <indesperate@outlook.com>
2023-11-29 16:19:46 +0800

    refactor directory

--- init.lua
+++ init.lua
@@ -2,20 +2,6 @@ require("basic")
 
 require("lazy-boot")
 
-require("plugins.ui")
-
-require("plugins.lsp")
-
-require("plugins.complete")
-
-require("plugins.highlight")
-
-require("plugins.obj")
-
-require("plugins.format-lint")
-
-require("plugins.git")
-
-require("plugins.mini")
+require("plugins")
 
 require("map")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,15 @@
+require("plugins.ui")
+
+require("plugins.lsp")
+
+require("plugins.complete")
+
+require("plugins.highlight")
+
+require("plugins.obj")
+
+require("plugins.format-lint")
+
+require("plugins.git")
+
+require("plugins.mini")
