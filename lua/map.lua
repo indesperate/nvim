2bc9ffa579982576e0723395863d5e731076b529
indesperate <indesperate@outlook.com>
2024-01-09 22:28:25 +0800

    add keys to tabs

--- lua/map.lua
+++ lua/map.lua
@@ -28,6 +28,12 @@ map("n", "gr", vim.lsp.buf.references, { desc = "Lsp refer", silent = true })
 map("n", "gd", vim.lsp.buf.definition, { desc = "Lsp def", silent = true })
 map("n", "gi", vim.lsp.buf.implementation, { desc = "Lsp impl", silent = true })
 
+-- tab manipulate
+map("n", "<c-n>c", "<cmd>tabnew<cr>", { desc = "New tab", silent = true})
+map("n", "<c-n>n", "<cmd>tabnext<cr>", { desc = "Next tab", silent = true})
+map("n", "<c-n>p", "<cmd>tabprevious<cr>", { desc = "Previous tab", silent = true})
+map("n", "<c-n>x", "<cmd>tabclose<cr>", { desc = "Close tab", silent = true})
+
 -- terminal mappings
 map("t", "<c-n>", "<c-\\><c-n>", { desc = "Terminal escape", silent = true })
 map("t", "<c-h>", "<c-\\><c-n><c-w>h", { desc = "Terminal left", silent = true })
