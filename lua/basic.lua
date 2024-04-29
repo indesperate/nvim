fe827d70c6439977519151f2e73a3ca29f4f4985
indesperate <indesperate@outlook.com>
2024-04-29 14:40:54 +0800

    use new inlay_hint api

--- lua/basic.lua
+++ lua/basic.lua
@@ -190,7 +190,7 @@ autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client.server_capabilities.inlayHintProvider then
 			if vim.lsp.inlay_hint then
-				vim.lsp.inlay_hint.enable(args.buf, true)
+				vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
 			end
 		end
 	end,
