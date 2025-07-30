08b2e3997a4761511b1352414aacbf4122f14fb3
indesperate <indesperate@outlook.com>
2025-07-30 14:00:05 +0800

    add markdown rendor support

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -21,4 +21,9 @@ return {
 		cmd = "WhichKey",
 		opts = {},
 	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		opts = {},
+	},
 }
