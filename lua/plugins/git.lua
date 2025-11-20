5d716630c1fa11b3e39fee732c5d031f5095c5ef
indesperate <indesperate@outlook.com>
2025-11-20 23:44:20 +0800

    add flash.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -9,16 +9,39 @@ return {
                 vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
             end
 
-            map("n", "]c", function()
-                gs.nav_hunk("next")
-            end, "Next Hunk")
-            map("n", "[c", function()
-                gs.nav_hunk("prev")
-            end, "Prev Hunk")
-            map("n", "du", gs.stage_hunk, "Toggle Stage Hunk")
-            map("n", "dp", gs.reset_hunk, "Reset Hunk")
+            local function obtain()
+                if vim.wo.diff then
+                    vim.cmd.normal({ "do", bang = true })
+                else
+                    gs.stage_hunk()
+                end
+            end
+            local function put()
+                if vim.wo.diff then
+                    vim.cmd.normal({ "dp", bang = true })
+                else
+                    gs.reset_hunk()
+                end
+            end
+            local function next_change()
+                if vim.wo.diff then
+                    vim.cmd.normal({ "]c", bang = true })
+                else
+                    gs.nav_hunk("next")
+                end
+            end
+            local function prev_change()
+                if vim.wo.diff then
+                    vim.cmd.normal({ "[c", bang = true })
+                else
+                    gs.nav_hunk("prev")
+                end
+            end
+            map("n", "]c", next_change, "Next Change")
+            map("n", "[c", prev_change, "Prev Change")
+            map("n", "do", obtain, "Obtain Diff")
+            map("n", "dp", put, "Put Diff")
             map("n", "dO", gs.preview_hunk_inline, "Preview Hunk Inline")
-            map("n", "do", gs.preview_hunk, "Preview Hunk")
             map("n", "db", gs.diffthis, "Diff This")
             map({ "o", "x" }, "ic", gs.select_hunk, "GitSigns Select Hunk")
         end,
