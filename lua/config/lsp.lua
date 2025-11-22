eff4a8bdcd35d9766fc78452b16e4e8759f3d984
indesperate <indesperate@outlook.com>
2025-11-22 12:48:11 +0800

    add autocmd codelens inlayhints

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -8,3 +8,24 @@ vim.lsp.enable("clangd")
 vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("ruff")
 vim.lsp.enable("lua_ls")
+
+local autocmd = vim.api.nvim_create_autocmd
+-- codelens and inlay hints
+autocmd("LspAttach", {
+    callback = function(args)
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+        if client:supports_method("textDocument/codeLens") then
+            vim.lsp.codelens.refresh()
+            vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave" }, {
+                buffer = bufnr,
+                callback = vim.lsp.codelens.refresh,
+            })
+        end
+
+        if client:supports_method("textDocument/inlayHint") then
+            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+        end
+    end,
+})
