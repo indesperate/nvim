735f93d67ff9c56af7837e1cbe84a4890dde6800
indesperate <indesperate@outlook.com>
2025-11-25 21:41:31 +0800

    using ty lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,12 +2,13 @@
 vim.diagnostic.config({ virtual_text = true })
 
 -- lsp config
-vim.lsp.enable("basedpyright")
 vim.lsp.enable("bashls")
+vim.lsp.enable("neocmake")
 vim.lsp.enable("clangd")
 vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("ruff")
 vim.lsp.enable("lua_ls")
+vim.lsp.enable("ty")
 vim.lsp.enable("codebook")
 
 local autocmd = vim.api.nvim_create_autocmd
